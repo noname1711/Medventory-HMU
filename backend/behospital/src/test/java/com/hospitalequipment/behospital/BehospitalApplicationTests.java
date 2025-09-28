12ffc5edb20f814cfa8c7a811c025f388860ac70
HungLe <le771485@gmail.com>
2025-09-28 21:26:13 +0700

    first demo

---
+++ backend/behospital/src/test/java/com/hospitalequipment/behospital/BehospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospitalequipment.behospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BehospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
