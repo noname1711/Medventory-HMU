8849a24a03dd51880d8ba88c83e3a6250fdbd08f
HungLe <le771485@gmail.com>
2025-10-12 00:28:58 +0700

    demo

--- backend/src/test/java/com/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.hospitalequipment.behospital;
+package com.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BehospitalApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
